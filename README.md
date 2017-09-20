a5a79cf3e53e2d4990b4f406272b9cd288516447
naoya <guchio@logos.t.u-tokyo.ac.jp>
2017-09-20 17:16:18 +0900

    finished init codings.

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # bAbIPrep
 The script which preprocess bAbI dataset to vector type usable form
 
-# Caution
-The generated file will be huge size, so plz take care about your disk size.
-
 # Usage
-0. 
+0. PYTHONPATH=. python main.py -s datasets/en-valid-10k -t datasets/preped
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import sys
 import argparse
 
-from utils import *
+from utils.tools import prepBAbI
 
 
 def getArgs():
@@ -16,14 +16,23 @@ def getArgs():
     '''
     parser = argparse.ArgumentParser(description="The script which preprocess bAbI dataset \
         to vector type usable form.")
-    parser.add_argument("-i", "--input_file", help="", required=True)
+    parser.add_argument("-s", "--source_dir", help="the source directory in which you have \
+            bAbI datasets.", required=True)
+    parser.add_argument("-t", "--target_dir", help="the target directory in which you wanna \
+            save preprocessed dataset.", required=True)
     args = parser.parse_args() 
+    if args.source_dir[-1] != '/':
+        args.source_dir += '/'
+    if args.target_dir[-1] != '/':
+        args.target_dir += '/'
 
     return args
 
 
 def main():
     args = getArgs()
+    source_files = [args.source_dir + source_file for source_file in os.listdir(args.source_dir)]
+    prepBAbI(source_files, args.target_dir)
 
 
 if __name__ == '__main__':
